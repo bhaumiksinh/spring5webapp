c3fb149d96d0fc350be7df9216d117931f3deb52
bhaumiksinh.chavda <bhaumiksinh.chavda@eclinicalworks.com>
2021-12-07 05:25:05 -0500

    added command liner interface

---
+++ src/main/java/bootstrap/BootStrapData.java
@@ -0,0 +1,54 @@
+package bootstrap;
+
+import domain.Author;
+import domain.Book;
+import domain.Publisher;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import repositories.AuthorRepository;
+import repositories.BookRepository;
+import repositories.PublisherRepository;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository=publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher pb=new Publisher("naranpura","ahmedabad","380013");
+        publisherRepository.save(pb);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Number of Books: " + bookRepository.count());
+    }
+}
